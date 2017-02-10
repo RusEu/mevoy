3779d5978c2039a9e27d8868dd85ca5d218e5eae
bidder <bidstopper@gmail.com>
2017-02-10 21:43:01 +0100

    Added login page

--- auth_account/views.py
+++ auth_account/views.py
@@ -1,5 +1,14 @@
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import FormView
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.core.urlresolvers import reverse
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login
+
+from company.models import Department
+
+from auth_account.forms import LoginForm
 
 
 class ProfilePageView(TemplateView):
@@ -8,3 +17,25 @@ class ProfilePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(ProfilePageView, self).get_context_data(**kwargs)
         return context
+
+
+class LoginUserPageView(FormView):
+    template_name = "auth_account/login.html"
+    form_class = LoginForm
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginUserPageView, self).get_context_data(**kwargs)
+        context["departments"] = Department.objects.all()
+        return context
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        department = form.cleaned_data["department"]
+        self.request.session["department"] = department.name
+        if self.request.user in department.employees.all():
+            self.request.session["is_employee"] = True
+            success_url = reverse('new_request')
+        if self.request.user in department.managers.all():
+            self.request.session["is_manager"] = True
+            success_url = reverse('pending_requests')
+        return HttpResponseRedirect(success_url)
--- mevoy/urls.py
+++ mevoy/urls.py
@@ -1,14 +1,21 @@
-from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import logout
+from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from auth_account.views import LoginUserPageView
+
 
 urlpatterns = [
+    url(r'^login/$', LoginUserPageView.as_view(), name='login'),
+    url(r'^logout/$', logout, {"next_page": "/login/"}),
+
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('auth_account.urls')),
     url(r'^vacantion_requests/', include('vacation_request.urls')),
     url(r'^calendar/', include('holiday_calendar.urls')),
     url(r'^notifications/', include('notifications.urls')),
     url('^', include('django.contrib.auth.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
