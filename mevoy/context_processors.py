9aecad772896273a5bd8271331d9640eccffe256
bidder <bidstopper@gmail.com>
2017-02-12 20:09:44 +0100

    cleaned context_processors

--- mevoy/context_processors.py
+++ mevoy/context_processors.py
@@ -1,6 +1,6 @@
 from django.contrib.sites.shortcuts import get_current_site
 
-from vacation_request.models import Request, RequestType
+from vacation_request.models import Request
 from notifications.models import Notification
 
 
@@ -8,30 +8,29 @@ def global_processor(request):
     if request.user.is_anonymous():
         return {}
 
-    notifications = request.user.notification_set.all().order_by('-datetime')
     context = {
         "SITE_NAME": get_current_site(request).name,
         "unknown_user_image": "http://www.wpclipart.com/signs_symbol/icons_oversized/male_user_icon.png",
-        "employee_notifications": notifications
     }
 
     department = request.session["department"]
-    requests = Request.objects.filter(department__name=department)
 
     # TODO: OPTIMIZE ME
     if request.session.get('is_employee'):
-        employee_requests = requests.filter(user=request.user)
+        employee = request.user
         context.update(dict(
-            employee_pending_requests=employee_requests.filter(status="pending"),
-            employee_approved_requests=employee_requests.filter(status="approved"),
-            employee_declined_requests=employee_requests.filter(status="declined")
+            employee_pending_requests=employee.pending_requests(department),
+            employee_approved_requests=employee.approved_requests(department),
+            employee_declined_requests=employee.declined_requests(department)
         ))
     if request.session.get('is_manager'):
+        requests = Request.objects.filter(department__name=department)
         context.update(dict(
             manager_pending_requests=requests.filter(status="pending"),
             manager_approved_requests=requests.filter(status="approved"),
             manager_declined_requests=requests.filter(status="declined"),
-            manager_notification=notifications.filter(department__name=department)
+            manager_notification=Notification.objects.filter(
+                department__name=department)
         ))
 
     return context
