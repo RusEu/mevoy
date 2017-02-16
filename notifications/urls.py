4da9317f3e38154b76a93ee37388a000e5c499da
bidder <bidstopper@gmail.com>
2017-02-16 23:29:40 +0100

    Removed unnecessary slash

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from notifications.views import NotificationsPageView
 
 urlpatterns = [
-    url(r'^/$', NotificationsPageView.as_view(), name='notifications'),
+    url(r'^$', NotificationsPageView.as_view(), name='notifications'),
 ]
