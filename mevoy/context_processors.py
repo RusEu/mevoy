614c0150bc87b57804cfaf56b44ea63eff13bc17
bidder <bidstopper@gmail.com>
2017-02-19 05:58:48 +0100

    Use context processor only if the user is manager

--- mevoy/context_processors.py
+++ mevoy/context_processors.py
@@ -13,25 +13,17 @@ def global_processor(request):
         "unknown_user_image": "http://www.wpclipart.com/signs_symbol/icons_oversized/male_user_icon.png",
     }
 
-    department = request.session["department"]
-
     if request.session.get('is_manager'):
-        requests = Request.objects.filter(department__name=department)
+        manager_departments = request.user.manager_departments.all()
+        requests = Request.objects.filter(department__in=manager_departments)
         request_types = RequestType.objects.filter(
             request__in=requests).distinct()
         context.update(dict(
-            department_request_types=request_types,
+            request_types=request_types,
             manager_pending_requests=requests.filter(status="pending"),
             manager_approved_requests=requests.filter(status="approved"),
             manager_declined_requests=requests.filter(status="declined"),
-            manager_notification=Notification.objects.filter(
-                department__name=department)
-        ))
-    else:
-        employee = request.user
-        context.update(dict(
-            employee_pending_requests=employee.pending_requests(department),
-            employee_approved_requests=employee.approved_requests(department),
-            employee_declined_requests=employee.declined_requests(department)
+            manager_notifications=Notification.objects.filter(
+                department__in=manager_departments)
         ))
     return context
