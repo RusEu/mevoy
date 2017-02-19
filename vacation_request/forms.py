cb20d1a4a54ab2184ebb3df132dd0901cbcd1320
bidder <bidstopper@gmail.com>
2017-02-19 06:02:27 +0100

    Added form to create a new request

--- vacation_request/forms.py
+++ vacation_request/forms.py
@@ -1,10 +1,47 @@
+import datetime
+
 from django import forms
+from django.utils.translation import ugettext as _
 
-from vacation_request.models import Request
+from auth_account.models import User
+
+from vacation_request.models import RequestType
 
 
-class RequestForm(forms.ModelForm):
-    class Meta:
-        model = Request
-        fields = ['user', 'start_date', 'end_date', 'description',
-                  'request_type', 'status']
+class RequestForm(forms.Form):
+    error_messages = {
+        "zero_days_left": _("You don't have days left for the type of request you chosen")
+    }
+
+    user = forms.ModelChoiceField(queryset=User.objects.all())
+    start_date = forms.CharField(max_length=100, required=True)
+    end_date = forms.CharField(max_length=100, required=True)
+    description = forms.CharField(max_length=250, required=False)
+    request_type = forms.ModelChoiceField(queryset=RequestType.objects.all())
+
+    def clean_start_date(self):
+        start_date = datetime.datetime.strptime(
+            self.cleaned_data.get('start_date'), '%Y-%m-%dT%H:%M')
+        return start_date
+
+    def clean_end_date(self):
+        end_date = datetime.datetime.strptime(
+            self.cleaned_data.get('end_date'), '%Y-%m-%dT%H:%M')
+        return end_date
+
+    def clean(self):
+        # If user does not have days left to make a request, raise error
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+        if start_date >= end_date:
+            #raise error
+            pass
+        requested_days = (end_date - start_date).days
+        request_type = self.cleaned_data.get('request_type')
+        user = self.cleaned_data.get('user')
+        days_left = user.days_left(request_type)["available_days"]
+        if days_left == 0 or days_left < requested_days:
+            raise forms.ValidationError(
+                self.error_messages["zero_days_left"],
+                code="zero_days_left"
+            )
--- vacation_request/views.py
+++ vacation_request/views.py
@@ -23,22 +23,33 @@ class PendingRequestsPageView(TemplateView):
         Notification.objects.create(user=vacation_request.user, message=message)
 
     def post(self, request, *args, **kwargs):
+        """
+        :return: HttpResponseForbidden if user is not manager or users's
+                 department is not the same as the request department or
+                 the action is not decline or approve.
+                 HttpResponse with 200 status if the request has been modified.
+        """
         data = request.POST
-        if request.session.get('is_manager'):
-            obj = Request.objects.get(id=data.get("request"))
-            if not request.session["department"] == obj.department.name:
-                return HttpResponseForbidden()
-            method = data["method"]
-            if method == 'approve':
-                obj.status = obj.APPROVED
-            elif method == 'decline':
-                obj.status = obj.DECLINED
+        action = data.get('action')
+        obj = Request.objects.get(id=data.get("request"))
+        if request.user not in obj.department.managers.all():
+            return HttpResponseForbidden()
+        if action not in ('approve', 'decline',):
+            return HttpResponseForbidden()
+
+        if action == 'approve':
+            requested_approvals = obj.request_type.approvals
+            obj.approvals.add(self.request.user)
+            if obj.department.managers.count() >= requested_approvals:
+                if obj.approvals.count() == requested_approvals:
+                    obj.status = obj.APPROVED
             else:
-                return HttpResponseForbidden()
-            self.notify_user(vacation_request=obj)
-            obj.save()
-            return HttpResponse(status=200)
-        return HttpResponseForbidden()
+                obj.status = obj.APPROVED
+        elif action == 'decline':
+            obj.status = obj.DECLINED
+        self.notify_user(vacation_request=obj)
+        obj.save()
+        return HttpResponse(status=200)
 
 
 class ApprovedRequestsPageView(TemplateView):
@@ -54,10 +65,31 @@ class NewRequestPageView(FormView):
     form_class = RequestForm
     success_url = reverse_lazy('pending_requests')
 
+    def get_form(self):
+        if self.request.POST:
+            data = self.request.POST.copy()
+            data["user"] = self.request.user.id
+            return self.form_class(data=data)
+        return self.form_class(**self.get_form_kwargs())
+
     def get_context_data(self, **kwargs):
         context = super(NewRequestPageView, self).get_context_data(**kwargs)
         return context
 
     def form_valid(self, form):
-        obj = form.save(commit=False)
-        obj.save()
+        Request.objects.create(
+            user=self.request.user,
+            start_date=form.cleaned_data.get('start_date'),
+            end_date=form.cleaned_data.get('end_date'),
+            description=form.cleaned_data.get('description'),
+            request_type=form.cleaned_data.get('request_type'),
+            department=self.request.user.employee_department,
+            status=Request.PENDING
+        )
+        Notification.objects.create(
+            department=self.request.user.employee_department,
+            message="User {} has made a new {} request".format(
+                self.request.user, form.cleaned_data.get('request_type').name
+            )
+        )
+        return super(NewRequestPageView, self).form_valid(form)
